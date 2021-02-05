1dc0e38daf33b2cc34ed0e3e8c2f80ac43eee613
kanaru0928 <kanaru0928@gmail.com>
2021-02-05 15:05:28 +0900

    Add lookahead mode

--- script.js
+++ script.js
@@ -12,38 +12,50 @@ var white;
 var startDate;
 
 function play(){
+	$('#start_btn').prop('disabled', true);
 	array = new BigUint64Array(buf);
 	w = 64;
 	h = 48;
 	black = $('#black').val();
 	white = $('#white').val();
 	lastFrame = 6570;
+	lookahead = $('#lookahead').prop('checked');
 	fps = 30;
 	// 読み込み
 	video = new Array(lastFrame);
 	let str;
 	let i = 0;
 	setTimeout(function tmp(){
-		str = "";
-		for(let j = 0; j < h; j++){
-			for(let k = 0; k < w; k++){
-				str += (array[i * h + j] >> BigInt(w - k - 1)) % 2n == 1 ? white : black;
-			}
-			str += "\n";
-		}
-		video[i] = str;
+		loadFrame(i);
+		i++;
 		if(i < lastFrame){
-			i++;
-			//let prog = Math.floor(i / lastFrame * 100) + '%';
-			//if($('#playarea') != prog){
-			//	$('#playarea').val(prog);
-			//}
+			if(lookahead){
+				let prog = Math.floor(i / lastFrame * 1000) / 10 + '%';
+				$('#playarea').val(prog);
+			}
 			setTimeout(tmp, 0);
+		}else if(lookahead){
+			startDate = Date.now();
+			tick();
+		}else{
+			console.log("finish load");
 		}
 	}, 0);
-	
-	startDate = Date.now();
-	tick();
+	if(!lookahead){
+		startDate = Date.now();
+		tick();
+	}
+}
+
+function loadFrame(i){
+	str = "";
+	for(let j = 0; j < h; j++){
+		for(let k = 0; k < w; k++){
+			str += (array[i * h + j] >> BigInt(w - k - 1)) % 2n == 1 ? white : black;
+		}
+		str += "\n";
+	}
+	video[i] = str;
 }
 
 function tick(){
@@ -51,6 +63,9 @@ function tick(){
 	else{
 		frame++;
 		let interval = frame / fps * 1000 + startDate - Date.now();
+		if(interval < 0){
+			frame = Math.floor((Date.now() - startDate) / 1000 * 30);
+		}
 		setTimeout(tick, interval);
 		$('#playarea').val(video[frame]);
 	}
